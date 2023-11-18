ec157a6d05e78d5da86d4e9277005006e5a09178
Malavi1 <pandemalavi7@gmail.com>
2023-11-18 10:04:43 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Registration from "./Registration";
+import Bot from "./bot";
+import Chat from "./chat";
+import { CookiesProvider, useCookies } from "react-cookie";
+function App() {
+  const [loggedInUsername, setLoggedInUsername] = useState(null);
+  const [navgation, setnavigation] = useState(true);
+  const [cookies, setCookie] = useCookies(["username"]);
+  useEffect(() => {
+    if (cookies.username) {
+      setnavigation(false);
+    }
+  }, [cookies]);
+  function handlenav(nav) {
+    setnavigation((nav) => !nav);
+  }
+  function handleLogin(username) {
+    setCookie("username", username, { path: "/" });
+  }
+
+  return (
+    <div>
+      {cookies.username ? (
+        <Chat />
+      ) : navgation ? (
+        <Login onhandlenav={handlenav} onLogin={handleLogin} />
+      ) : (
+        <Registration onhandlenav={handlenav} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Registration.js
@@ -0,0 +1,84 @@
+import "./App.css";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComments } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+
+function Registration({ onhandlenav }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleRegister = async () => {
+    const response = await fetch("http://localhost:5000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    if (data.error) {
+      setErrorMessage("Error during registration. Please try again.");
+    }
+  };
+  return (
+    <div className="main">
+      <div className="right">
+        <div className="login">
+          <FontAwesomeIcon icon={faComments} />
+          <p>User Registration</p>
+        </div>
+        <div className="login">
+          <div className="user-input">
+            {errorMessage && (
+              <p className="error" style={{ color: "red" }}>
+                Error while Fetching Data
+              </p>
+            )}
+            <h5>User ID</h5>
+            <div className="search-icon">
+              <FontAwesomeIcon icon={faUser} />
+              <input
+                placeholder="User Name"
+                className="user-input"
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="user-input">
+            <h5>Password</h5>
+            <div className="search-icon">
+              <FontAwesomeIcon icon={faLock} />
+              <input
+                placeholder="Password"
+                className="user-input"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="login">
+          <button className="button" onClick={handleRegister}>
+            Register
+          </button>
+          <a href="#">
+            Already Have Account!
+            <span className="highlight" onClick={onhandlenav}>
+              {" "}
+              Login
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Registration;
